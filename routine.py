3f560fb8e20dfb1b1f99f971da89603c4d216f40
yangli0534 <yangli0534@yahoo.com>
2017-07-16 23:32:56 +0800

    second

---
+++ routine.py
@@ -0,0 +1,15 @@
+#-*- coding: uft-8-*
+#!/usr/bin/python
+
+import socket
+import datetime
+import os
+import platform
+
+def run_task():
+    print "It is my schedue"
+def timerFun(sched_Timer):
+    flag = 0
+    while True:
+
+    
